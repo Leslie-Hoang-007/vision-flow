5465d1579a66b4eb7935d122f07e7e7646891310
Leslie-Hoang-007 <111301770+Leslie-Hoang-007@users.noreply.github.com>
2024-01-22 20:51:19 -0500

    init

--- src/App copy.js
+++ src/App copy.js
@@ -1,11 +1,10 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { FaceLandmarker, FilesetResolver, DrawingUtils } from "@mediapipe/tasks-vision";
 
 import './App.css';
 
 function App() {
-  const [faceLandmarker, setFaceLandmarker] = useState(null);
-  const [dBlendShapes, setDBlendShapes] = useState(null);
+  let faceLandmarker;
   let runningMode = "IMAGE";
   let enableWebcamButton;
   let webcamRunning = false;
@@ -19,7 +18,7 @@ function App() {
       const filesetResolver = await FilesetResolver.forVisionTasks(
         "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm"
       );
-      const landmarker = await FaceLandmarker.createFromOptions(filesetResolver, {
+      faceLandmarker = await FaceLandmarker.createFromOptions(filesetResolver, {
         baseOptions: {
           modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task`,
           delegate: "GPU"
@@ -28,12 +27,89 @@ function App() {
         runningMode,
         numFaces: 1
       });
-      setFaceLandmarker(landmarker);
     }
     createFaceLandmarker();
-
   }, []);
 
+  const handleClick = async (event) => {
+    if (!faceLandmarker) {
+      console.log("Wait for faceLandmarker to load before clicking!");
+      return;
+    }
+
+    if (runningMode === "VIDEO") {
+      runningMode = "IMAGE";
+      await faceLandmarker.setOptions({ runningMode });
+    }
+
+    const allCanvas = event.target.parentNode.getElementsByClassName("canvas");
+    for (let i = allCanvas.length - 1; i >= 0; i--) {
+      const n = allCanvas[i];
+      n.parentNode.removeChild(n);
+    }
+
+    const faceLandmarkerResult = await faceLandmarker.detect(event.target);
+    const canvas = document.createElement("canvas");
+    canvas.setAttribute("class", "canvas");
+    canvas.setAttribute("width", event.target.naturalWidth + "px");
+    canvas.setAttribute("height", event.target.naturalHeight + "px");
+    canvas.style.left = "0px";
+    canvas.style.top = "0px";
+    canvas.style.width = `${event.target.width}px`;
+    canvas.style.height = `${event.target.height}px`;
+
+    event.target.parentNode.appendChild(canvas);
+    const ctx = canvas.getContext("2d");
+    const drawingUtils = new DrawingUtils(ctx);
+    for (const landmarks of faceLandmarkerResult.faceLandmarks) {
+      drawingUtils.drawConnectors(
+        landmarks,
+        FaceLandmarker.FACE_LANDMARKS_TESSELATION,
+        { color: "#C0C0C070", lineWidth: 1 }
+      );
+      drawingUtils.drawConnectors(
+        landmarks,
+        FaceLandmarker.FACE_LANDMARKS_RIGHT_EYE,
+        { color: "#FF3030" }
+      );
+      drawingUtils.drawConnectors(
+        landmarks,
+        FaceLandmarker.FACE_LANDMARKS_RIGHT_EYEBROW,
+        { color: "#FF3030" }
+      );
+      drawingUtils.drawConnectors(
+        landmarks,
+        FaceLandmarker.FACE_LANDMARKS_LEFT_EYE,
+        { color: "#30FF30" }
+      );
+      drawingUtils.drawConnectors(
+        landmarks,
+        FaceLandmarker.FACE_LANDMARKS_LEFT_EYEBROW,
+        { color: "#30FF30" }
+      );
+      drawingUtils.drawConnectors(
+        landmarks,
+        FaceLandmarker.FACE_LANDMARKS_FACE_OVAL,
+        { color: "#E0E0E0" }
+      );
+      drawingUtils.drawConnectors(
+        landmarks,
+        FaceLandmarker.FACE_LANDMARKS_LIPS,
+        { color: "#E0E0E0" }
+      );
+      drawingUtils.drawConnectors(
+        landmarks,
+        FaceLandmarker.FACE_LANDMARKS_RIGHT_IRIS,
+        { color: "#FF3030" }
+      );
+      drawingUtils.drawConnectors(
+        landmarks,
+        FaceLandmarker.FACE_LANDMARKS_LEFT_IRIS,
+        { color: "#30FF30" }
+      );
+    }
+    drawBlendShapes(canvasRef.current, faceLandmarkerResult.faceBlendshapes);
+  };
 
   const enableCam = async (event) => {
     if (!faceLandmarker) {
@@ -43,10 +119,10 @@ function App() {
 
     if (webcamRunning === true) {
       webcamRunning = false;
-      // enableWebcamButton.innerText = "ENABLE PREDICTIONS";
+      enableWebcamButton.innerText = "ENABLE PREDICTIONS";
     } else {
       webcamRunning = true;
-      // enableWebcamButton.innerText = "DISABLE PREDICTIONS";
+      enableWebcamButton.innerText = "DISABLE PREDICTIONS";
     }
 
     const constraints = {
@@ -142,132 +218,44 @@ function App() {
     let htmlMaker = "";
     blendShapes[0].categories.map((shape) => {
       htmlMaker += `
-      <li class="blend-shapes-item">
-      <p class="blend-shapes-label">${shape.displayName || shape.categoryName}</p>
-      <p class="blend-shapes-value">${(+shape.score).toFixed(4)}</p>
-      </li>
+        <li class="blend-shapes-item">
+          <span class="blend-shapes-label">${shape.displayName || shape.categoryName}</span>
+          <span class="blend-shapes-value" style="width: calc(${+shape.score * 100}% - 120px)">${(+shape.score).toFixed(4)}</span>
+        </li>
       `;
     });
 
-    setDBlendShapes(blendShapes);
+    el.innerHTML = htmlMaker;
   };
-
-  const [lastActionTime, setLastActionTime] = useState(0);
-
-
-  const downArrowEvent = new KeyboardEvent('keydown', {
-    key: 'ArrowDown',
-    code: 'ArrowDown',
-    keyCode: 40,
-    which: 40,
-  });
-
-  const dispatchDownArrowEvent = () => {
-    const currentTime = Math.floor(new Date().getTime());
-    setLastActionTime(currentTime);
-    // document.dispatchEvent(downArrowEvent);
-
-    document.getElementById('if1').scrollTop = 438
-    console.log("down");
-    // scrollDown();
-    const iframe = document.getElementById('if1'); // Replace 'if1' with the actual ID of your iframe
-    // iframe.contentWindow.document.dispatchEvent(downArrowEvent);
-  };
-
-  // const scrollDown = () => {
-  //   window.scrollBy(0, 100); // Adjust the value based on how much you want to scroll down
-  // };
-  const scrollDown = () => {
-    const iframe = document.getElementById('if1'); // Replace 'if1' with the actual ID of your iframe
-
-    iframe.contentWindow.scrollTop = 438 // Adjust the value based on how much you want to scroll down
-
-  };
-
-  const render = () => {
-    if (dBlendShapes != null) {
-      // const data = dBlendShapes[0].categories.map((shape) => (
-      //   <div key={shape.displayName || shape.categoryName}>
-      //     <li className="blend-shapes-item">
-      //       <p className="blend-shapes-label">{shape.displayName || shape.categoryName}</p>
-      //       {/* <span className="blend-shapes-value" style={{ width: `calc(${(+shape.score) * 100}% - 120px)` }}/> */}
-
-      //       <p>        {(shape.score).toFixed(4)}</p>
-      //     </li>
-      //   </div>
-      // ));
-
-
-      const data = () => {
-
-        return (
-          <div>
-            <li>
-              <p>{dBlendShapes[0].categories[11].categoryName} <span className="blend-shapes-value" style={{ width: `calc(${(dBlendShapes[0].categories[11].score) * 100}% - 120px)` }} /> </p>
-              <p>{dBlendShapes[0].categories[11].score}</p>
-              {Math.floor(new Date().getTime()) > (lastActionTime + 1000) && dBlendShapes[0].categories[11].score > 0.5 ? dispatchDownArrowEvent() : ""}
-            </li>
-            <li>
-              <p>{dBlendShapes[0].categories[12].categoryName} <span className="blend-shapes-value" style={{ width: `calc(${(dBlendShapes[0].categories[12].score) * 100}% - 120px)` }} /> </p>
-              <p>{dBlendShapes[0].categories[12].score}</p>
-            </li>
-            <li>
-              <p>{dBlendShapes[0].categories[17].categoryName} <span className="blend-shapes-value" style={{ width: `calc(${(dBlendShapes[0].categories[17].score) * 100}% )` }} /> </p>
-              <p>{dBlendShapes[0].categories[17].score}</p>
-            </li>
-            <li>
-              <p>{dBlendShapes[0].categories[18].categoryName} <span className="blend-shapes-value" style={{ width: `calc(${(dBlendShapes[0].categories[18].score) * 100}% )` }} /> </p>
-              <p>{dBlendShapes[0].categories[18].score}</p>
-            </li>
-          </div>
-        )
-      }
-      // console.log(dBlendShapes[0].categories[11].categoryName);
-      // console.log(dBlendShapes[0].categories[12].categoryName);
-      return (
-        <div>
-          {data()}
-        </div>
-      );
-    }
-  };
-
-
 
   return (
     <div>
       <h1>Face landmark detection using the MediaPipe FaceLandmarker task</h1>
 
       <section id="demos" className="invisible">
+        <h2>Demo: Detecting Images</h2>
+        <p><b>Click on an image below</b> to see the key landmarks of the face.</p>
 
+        <div className="detectOnClick" onClick={handleClick}>
+          <img src="https://storage.googleapis.com/mediapipe-assets/portrait.jpg" width="100%" alt="portrait" />
+        </div>
         <div className="blend-shapes">
           <ul className="blend-shapes-list" id="image-blend-shapes"></ul>
         </div>
 
+        <h2>Demo: Webcam continuous face landmarks detection</h2>
         <p>Hold your face in front of your webcam to get real-time face landmarker detection.</p>
         <button id="webcamButton" className="mdc-button mdc-button--raised" onClick={enableCam}>
           <span className="mdc-button__ripple"></span>
           <span className="mdc-button__label">{webcamRunning ? "DISABLE PREDICTIONS" : "ENABLE WEBCAM"}</span>
         </button>
-
         <div style={{ position: "relative" }}>
-          <video id="webcam" style={{ position: "reletative", left: "0px", top: "0px" }} ref={videoRef} autoPlay playsInline></video>
+          <video id="webcam" style={{ position: "absolute", left: "0px", top: "0px" }} ref={videoRef} autoPlay playsInline></video>
           <canvas className="output_canvas" id="output_canvas" style={{ position: "absolute", left: "0px", top: "0px" }} ref={canvasRef}></canvas>
         </div>
         <div className="blend-shapes">
-          <ul className="blend-shapes-list" id="video-blend-shapes">
-            {render()}
-          </ul>
-
+          <ul className="blend-shapes-list" id="video-blend-shapes"></ul>
         </div>
-        <iframe width="315" height="560"
-          src="https://www.youtube.com/shorts/MbwTV4Tj_6o"
-          title="YouTube video player"
-          frameborder="0"
-          allowfullscreen></iframe>
-
-        <iframe id="if1" width="100%" allowScriptAccess="always" height="500px" src=" https://www.google.com/search?igu=1" frameborder="0" allowfullscreen></iframe>
-
       </section>
     </div>
   );
